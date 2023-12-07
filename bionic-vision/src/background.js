981ae5d53bc90ce394f70f15afe9721fa7ccd8d7
Lilli Nappi <116825146+nappilil@users.noreply.github.com>
2023-12-06 21:17:50 -0500

    Update background.js

--- bionic-vision/src/background.js
+++ bionic-vision/src/background.js
@@ -1,6 +1,5 @@
 import {
   bionify,
-  //patternsInclude,
   defaultHighlightSheet,
   defaultRestSheet,
   defaultAlgorithm,
@@ -11,36 +10,12 @@ chrome.runtime.onInstalled.addListener(() => {
     highlightSheet: defaultHighlightSheet,
     restSheet: defaultRestSheet,
     autoApply: false,
-    //excludedPatterns: [],
     algorithm: defaultAlgorithm,
   });
 });
-/** 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-  if (changeInfo.status == "complete") {
-    chrome.storage.sync.get(["autoApply", "excludedPatterns"], async (data) => {
-      if (data.autoApply) {
-        let tab = await chrome.tabs.get(tabId);
-        if (!patternsInclude(data.excludedPatterns, tab.url)) {
-          chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            function: bionify,
-          });
-        }
-      }
-    });
-  }
-});
-*/
+
 
 chrome.commands.onCommand.addListener(async (command) => {
-  /**  
-  if (command === "toggle-auto-bionify") {
-    chrome.storage.sync.get(["autoApply"], (data) => {
-      chrome.storage.sync.set({ autoApply: !data.autoApply });
-    });
-  }
-  */
   if (command === "toggle-bionify") {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
